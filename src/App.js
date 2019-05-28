47ecb8840d3498b6ed6488b87e16e98ffbf8bfe7
phomza <pasha.gmz@gmail.com>
2019-05-28 23:48:41 +0300

    react jsx

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+export default class App extends React.Component{
+    render() {
+        return <React.Fragment><div><p>Hello from App component</p></div></React.Fragment>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import {render} from 'react-dom'
 import AppService from './modules/app.service'
 import {config} from './modules/config'
 import './modules/header.component'
@@ -5,8 +7,11 @@ import './css/index.css'
 import './css/header.css'
 import './less/index.less'
 import './scss/index.scss'
+import App from './App'
 
 console.log('Config key:', config.key);
 
 const service = new AppService('Hello world!');
 service.log();
+
+render(<App></App>, document.getElementById("app"));
