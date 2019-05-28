817f7526e2765fdaf48304149f75b679ed036d35
phomza <pasha.gmz@gmail.com>
2019-05-28 21:43:10 +0300

    move styles to file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { config } from './modules/config'
 import './modules/header.component'
 import css from './css/index.css'
+import './css/header.css'
 
 console.log(css);
 console.log("Key: " + config.key);
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,36 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, "dist")
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin({}),
+            // new UglifyJsPlugin({})
+        ]
     },
     plugins: [
         new HTMLPlugin({
             filename: "index.html",
             template: "./src/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css"
         })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
         ]
     }
-
 };
